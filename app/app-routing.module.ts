4e85ba3155f5890fe1e6843d1cff428112c34753
Marcos José Cecotti <marcosjcecotti@hotmail.com>
2021-12-06 21:48:33 -0300

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { AuthGuard } from './services/auth.guard';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent,
+    pathMatch: 'full',
+    canActivate:[AuthGuard]
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
