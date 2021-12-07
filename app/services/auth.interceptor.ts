2a91a6bdaab32c167c1519601431037f6d1ce511
Marcos José Cecotti <marcosjcecotti@hotmail.com>
2021-12-06 21:49:25 -0300

    Add files via upload

---
+++ app/services/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { LoginService } from './login.service';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+
+  constructor(private loginService:LoginService){
+
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    let newReq = req;
+    let token = this.loginService.getToken();
+
+    console.log("INTERCEPTOR ",token);
+    if(token!=null){
+      newReq.clone({setHeaders:{Authorization:'Bearer ${token}'}})
+    }
+    return next.handle(newReq);
+
+  }
+
+}
